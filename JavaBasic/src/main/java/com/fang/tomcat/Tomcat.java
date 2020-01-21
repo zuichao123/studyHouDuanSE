16443d286f34af43d388caaea8278650f106143b
zuichao <zuichao123@163.com>
2020-01-21 16:03:18 +0800

    update in 2020-01-21 16:02:30

--- JavaBasic/src/main/java/com/fang/tomcat/Tomcat.java
+++ JavaBasic/src/main/java/com/fang/tomcat/Tomcat.java
@@ -2,6 +2,20 @@ package com.fang.tomcat;
 
 /**
  *Tomcat
+ *      Tomcat是一个web应用服务，web应用服务器是运行在服务器主机上的一个服务，
+ *      作用是将可以被客户端访问到的资源存在文本应用服务中，启动服务，客户端可以通过
+ *      访问该服务来获取资源，除此之外的资源客户端时无法直接访问的，就保证了服务器的安全性。
+ *
+ *      web应用服务：tomcat、jboss、weblogic、jetty
+ *
+ *      tomcat不用安装，解压之后可以直接使用。
+ *          bin：存放不同平台下（Windows Linux）启动和关闭Tomcat服务的脚本文件
+ *          conf：存放tomcat的各种配置文件
+ *          lib：存放tomcat服务需要的jar
+ *          logs：存放tomcat运行时存放的临时文件
+ *          temp：tomcat运行时存放的临时文件
+ *          webapps：存放允许客户端访问的资源
+ *          work：存放JSP文件转换后的Servlet文件
  *
  */
 public class Tomcat {
